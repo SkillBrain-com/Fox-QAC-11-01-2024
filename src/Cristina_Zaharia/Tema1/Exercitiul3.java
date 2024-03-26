100c0dbb502bf5f98f2d8e2b7448db637e1e921c
CristinaZaharia19 <cristina.zaharia.2019@gmail.com>
2024-03-26 21:20:04 +0200

    Am adaugat Tema1_ex3

---
+++ src/Cristina_Zaharia/Tema1/Exercitiul3.java
@@ -0,0 +1,9 @@
+package Cristina_Zaharia.Tema1;
+
+public class Exercitiul3 {
+    public static void main(String[] args) {
+        int Ana = 24;
+        int David = Ana - 6;
+        System.out.println(David);
+    }
+}
