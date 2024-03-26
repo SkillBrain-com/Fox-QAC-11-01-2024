d5f74c6ecd911f4d7a198de61dfe6b57c656475f
CristinaZaharia19 <cristina.zaharia.2019@gmail.com>
2024-03-26 21:21:54 +0200

    Am adaugat Tema1_ex4

---
+++ src/Cristina_Zaharia/Tema1/Exercitiul4.java
@@ -0,0 +1,10 @@
+package Cristina_Zaharia.Tema1;
+import java.util.Scanner;
+public class Exercitiul4 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Introduceti o propozitie");
+        String propozitie = scanner.nextLine();
+        System.out.println(propozitie);
+    }
+}
