bf013fc21775e1d4340a1e3902fab0b458ef7f3a
CristinaZaharia19 <cristina.zaharia.2019@gmail.com>
2024-03-26 21:27:26 +0200

    Am adaugat Tema1_ex7

---
+++ src/Cristina_Zaharia/Tema1/Exercitiul7.java
@@ -0,0 +1,13 @@
+package Cristina_Zaharia.Tema1;
+import java.util.Scanner;
+public class Exercitiul7 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        double numar = scanner.nextDouble();
+        System.out.println(numar + 5);
+        System.out.println(numar - 12.3);
+        System.out.println(numar * -3.2);
+        System.out.println(numar / 4);
+        System.out.println(numar % 6);
+    }
+}
