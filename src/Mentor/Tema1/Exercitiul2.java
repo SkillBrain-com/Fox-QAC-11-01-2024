86b489312e0f73aad38a8ffa14550535436b7e2e
andreisoare21 <andrei.daniel.soare@gmail.com>
2024-03-26 20:58:11 +0200

    Am adaugat exercitiul 2

---
+++ src/Mentor/Tema1/Exercitiul2.java
@@ -0,0 +1,12 @@
+package Mentor.Tema1;
+
+public class Exercitiul2 {
+    public static void main(String[] args){
+        System.out.println("Cursul acesta este foarte fain!");
+        System.out.println("Cursul acesta este foarte fain!");
+        System.out.println("Cursul acesta este foarte fain!");
+        System.out.println("Cursul acesta este foarte fain!");
+        System.out.println("Cursul acesta este foarte fain!");
+        System.out.println("Cursul acesta este foarte fain!");
+    }
+}
