d9b1a4757261c691e1c24e6c2a6e05ffc4bbf101
BiancaJianu <bianca.forray@yahoo.com>
2024-03-26 20:58:17 +0200

    am adugat tema 1 exercitiul 2

---
+++ src/BiancaJianu/Tema1/Exercitiul2.java
@@ -0,0 +1,12 @@
+package BiancaJianu.Tema1;
+
+public class Exercitiul2 {
+    public static void main(String[] args) {
+        System.out.println("Cursul acesta este foarte fain!");
+        System.out.println("Cursul acesta este foarte fain!");
+        System.out.println("Cursul acesta este foarte fain!");
+        System.out.println("Cursul acesta este foarte fain!");
+        System.out.println("Cursul acesta este foarte fain!");
+        System.out.println("Cursul acesta este foarte fain!");
+    }
+}
