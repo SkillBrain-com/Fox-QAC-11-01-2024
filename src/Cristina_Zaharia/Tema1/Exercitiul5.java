09d7f910d15ea5ac0fa9efe84fc1875a7a65bbb6
CristinaZaharia19 <cristina.zaharia.2019@gmail.com>
2024-03-26 21:25:20 +0200

    Am adaugat Tema1_ex5

---
+++ src/Cristina_Zaharia/Tema1/Exercitiul5.java
@@ -0,0 +1,15 @@
+package Cristina_Zaharia.Tema1;
+
+public class Exercitiul5 {
+    public static void main(String[] args) {
+            myVariables();
+        }
+
+        public static void myVariables(){
+            int a = 24;
+            String b = "Java";
+            boolean c = true;
+
+            System.out.println(a + " " + b + " " + c);
+        }
+}
