1ff34632f58c90110b06eef6a4b65bdbc597d779
CristinaZaharia19 <cristina.zaharia.2019@gmail.com>
2024-03-26 20:58:13 +0200

    Am adaugat Tema1_ex2

---
+++ src/Cristina_Zaharia/Tema1/Exercitiul2.java
@@ -0,0 +1,12 @@
+package Cristina_Zaharia.Tema1;
+
+public class Exercitiul2 {
+    public static void main(String[] args) {
+        System.out.println("Cursul acesta este foarte fain!");
+        System.out.println("Cursul acesta este foarte fain!");
+        System.out.println("Cursul acesta este foarte fain!");
+        System.out.println("Cursul acesta este foarte fain!");
+        System.out.println("Cursul acesta este foarte fain!");
+        System.out.println("Cursul acesta este foarte fain!");
+    }
+}
