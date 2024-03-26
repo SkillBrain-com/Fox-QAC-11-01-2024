d320bf4dbaf78a50214575af3497e0147e19f511
mariamoldovan26 <maria.moldovan.gtm@gmail.com>
2024-03-26 20:58:23 +0200

    Am adaugat exercitiul 2

---
+++ src/MariaMoldovan/Tema1/Exercitiul2.java
@@ -0,0 +1,12 @@
+package MariaMoldovan.Tema1;
+
+public class Exercitiul2 {
+    public static void main(String[] args) {
+        System.out.println("Cursul aceta este foarte fain!");
+        System.out.println("Cursul aceta este foarte fain!");
+        System.out.println("Cursul aceta este foarte fain!");
+        System.out.println("Cursul aceta este foarte fain!");
+        System.out.println("Cursul aceta este foarte fain!");
+        System.out.println("Cursul aceta este foarte fain!");
+    }
+}
