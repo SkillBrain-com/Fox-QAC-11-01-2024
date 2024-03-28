7092edb175a8e4a31a2c5664fe88ce94b8b22e82
DianaMadalinaL <diana.morar94@yahoo.com>
2024-03-28 16:30:36 +0200

    Am adaugat tema 3 exercitiul BirdWatcher

--- src/DianaLuca/Tema3/BirdWatcher.java
+++ src/DianaLuca/Tema3/BirdWatcher.java
@@ -1,49 +1,49 @@
 package DianaLuca.Tema3;
 
 class BirdWatcher {
-    private final int[] birdsPerDay;
+        private final int[] birdsPerDay;
 
-    public BirdWatcher(int[] birdsPerDay) {
-        this.birdsPerDay = birdsPerDay.clone();
-    }
-
-    public int[] getLastWeek() {
-        return new int[]{0, 2, 5, 3, 7, 8, 4};
-    }
-
-    public int getToday() {
-        if(birdsPerDay.length == 0) return 0;
-        return birdsPerDay[birdsPerDay.length - 1];
-    }
-
-    public void incrementTodaysCount() {
-        if(birdsPerDay.length > 0)
-            birdsPerDay[birdsPerDay.length - 1]++ ;
-    }
-
-    public boolean hasDayWithoutBirds() {
-        for( int i : birdsPerDay){
-            if(i==0)
-                return true;
+        public BirdWatcher(int[] birdsPerDay) {
+            this.birdsPerDay = birdsPerDay.clone();
         }
-        return false;
-    }
-    public int getCountForFirstDays(int numberOfDays) {
-        int vis = 0;
-        if(numberOfDays >= birdsPerDay.length)
-            numberOfDays = birdsPerDay.length;
-        if(birdsPerDay.length != 0)
-            for(int i = 0; i < numberOfDays; i++){
-                vis +=birdsPerDay[i];
+
+        public int[] getLastWeek() {
+            return new int[]{0, 2, 5, 3, 7, 8, 4};
+        }
+
+        public int getToday() {
+            if(birdsPerDay.length == 0) return 0;
+            return birdsPerDay[birdsPerDay.length - 1];
+        }
+
+        public void incrementTodaysCount() {
+            if(birdsPerDay.length > 0)
+                birdsPerDay[birdsPerDay.length - 1]++ ;
+        }
+
+        public boolean hasDayWithoutBirds() {
+            for( int i : birdsPerDay){
+                if(i==0)
+                    return true;
             }
-        return vis;
-    }
-    public int getBusyDays() {
-        int vis  = 0;
-        for(int i : birdsPerDay){
-            if(i >= 5)
-                vis++;
+            return false;
         }
-        return vis;
-    }
+        public int getCountForFirstDays(int numberOfDays) {
+            int vis = 0;
+            if(numberOfDays >= birdsPerDay.length)
+                numberOfDays = birdsPerDay.length;
+            if(birdsPerDay.length != 0)
+                for(int i = 0; i < numberOfDays; i++){
+                    vis +=birdsPerDay[i];
+                }
+            return vis;
+        }
+        public int getBusyDays() {
+            int vis  = 0;
+            for(int i : birdsPerDay){
+                if(i >= 5)
+                    vis++;
+            }
+            return vis;
+        }
 }
