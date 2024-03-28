488011fb3076bcb694f2360f80b8e5a0e562656f
DianaMadalinaL <diana.morar94@yahoo.com>
2024-03-28 16:15:09 +0200

    Am adaugat tema 2 exercitiul 2

--- src/DianaLuca/Tema2/Exercitiul2.java
+++ src/DianaLuca/Tema2/Exercitiul2.java
@@ -1,8 +1,8 @@
 package DianaLuca.Tema2;
+
 import java.util.Scanner;
 
-public class Exercitiul2T2 {
-
+public class Exercitiul2 {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
