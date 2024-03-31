f8e2c8072b6fe20359f8344fb5d192435a3b59d5
NicolaDenisa <nicoladenisa97@gmail.com>
2024-03-31 16:49:15 +0300

    Am adaugat exercitiul 2

---
+++ src/NicolaDenisa/Tema1/Exercitiul2.java
@@ -0,0 +1,12 @@
+package NicolaDenisa.Tema1;
+
+public class Exercitiul2 {
+    public static void main(String[] args) {
+        System.out.println("Cursul acesta este foarte fain ");
+        System.out.println("Cursul acesta este foarte fain ");
+        System.out.println("Cursul acesta este foarte fain ");
+        System.out.println("Cursul acesta este foarte fain ");
+        System.out.println("Cursul acesta este foarte fain ");
+        System.out.println("Cursul acesta este foarte fain ");
+    }
+}
