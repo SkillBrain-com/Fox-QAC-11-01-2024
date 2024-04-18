9fb8d6a13ec6aa4dfc8b736e50fa4306c3043a6e
CristinaZaharia19 <cristina.zaharia.2019@gmail.com>
2024-04-18 21:46:00 +0300

    added several tests

--- src/main/java/HomePage.java
+++ src/main/java/HomePage.java
@@ -1,12 +1,19 @@
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.ui.WebDriverWait;
 import org.testng.Assert;
+
+import java.time.Duration;
 
 public class HomePage {
 
     WebDriver driver;
+    WebDriverWait wait;
+
     public HomePage(WebDriver driver) {
         this.driver = driver;
+        this.wait = new WebDriverWait(driver, Duration.ofSeconds(30));
     }
 
     //Elemente
@@ -16,8 +23,24 @@ public class HomePage {
     private By addToCart = By.cssSelector("div[id=\"1\"]>.shelf-item__buy-btn");
     private By checkoutButton = By.cssSelector(".buy-btn");
 
+
+    private By productName = By.cssSelector("div[id=\"1\"]>.shelf-item__title");
+    private By productPrice = By.cssSelector("div[id=\"1\"]>.shelf-item__price>.val>b");
+    private By productPriceDecimal = By.cssSelector("div[id=\"1\"]>.shelf-item__price>.val>span");
+    private By cartProductName = By.cssSelector(".shelf-item__details>.title");
+    private By cartProductPrice = By.cssSelector(".float-cart__shelf-container>.shelf-item>.shelf-item__price>p");
+    private By cartTotalPrice = By.cssSelector(".sub-price__val");
+
+
+    private By favouriteButon = By.cssSelector("div[id=\"1\"]>.shelf-stopper>.MuiButtonBase-root.MuiIconButton-root.Button");
+    private By favouriteButtonClicked = By.cssSelector("div[id=\"1\"]>.shelf-stopper>button");
+
+    private By goToFavouritesPage = By.cssSelector("#favourites");
+
+
+
     //Metodele cu pasii
-    public void verifyLogin(){
+    public void verifyLogin() {
         String username = driver.findElement(usernameSpan).getText();
         Assert.assertEquals(username, "demouser");
 
@@ -28,8 +51,52 @@ public class HomePage {
         Assert.assertEquals(url, "https://bstackdemo.com/?signin=true");
     }
 
-    public void addToCart(){
+    public void addToCart() {
         driver.findElement(addToCart).click();
+    }
+
+    public void checkout(){
         driver.findElement(checkoutButton).click();
     }
+
+    public void verifyCart(){
+
+
+      String actualProductName = driver.findElement(productName).getText();
+      //wait.until(ExpectedConditions.textToBePresentInElement(cartProductName,actualProductName));
+
+      String actualCartProductName = driver.findElement(cartProductName).getText();
+      Assert.assertEquals(actualProductName, cartProductName);
+
+
+      String actualProductPrice = "$ " + driver.findElement(productPrice).getText() + driver.findElement(productPriceDecimal).getText();
+      //wait.until(ExpectedConditions.textToBePresentInElement(cartProductPrice, actualProductPrice));
+      String actualCartProductPrice = driver.findElement(cartProductPrice).getText();
+      String actualCartTotalPrice = driver.findElement(cartTotalPrice).getText();
+
+      Assert.assertEquals(actualProductPrice, actualCartProductPrice);
+      Assert.assertEquals(actualProductPrice, actualCartTotalPrice);
+    }
+
+    public void addToFavorites(){
+
+        driver.findElement(favouriteButon).click();
+    }
+
+    public void verifyFavouritesClicked(){
+       String attribute =  driver.findElement(favouriteButtonClicked).getAttribute("class");
+       boolean attributeIsPresent = attribute.contains("clicked");
+   //boolean ceva = driver.findElement(favouriteButtonClicked).getAttribute("class").contains("clicked");
+
+        Assert.assertTrue(attributeIsPresent);
+    }
+
+    public void goToFavourites(){
+        driver.findElement(goToFavouritesPage).click();
+    }
+
+    public WebElement extractProductName(){
+        return driver.findElement(productName);
+    }
+
 }
