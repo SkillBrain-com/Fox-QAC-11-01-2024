e3b5fef6e581397628b48657374121d4b55b310d
DianaMadalinaL <diana.morar94@yahoo.com>
2024-05-08 13:44:10 +0300

    teste Tool Shop

--- src/main/java/toolShopDemo/LoginPage.java
+++ src/main/java/toolShopDemo/LoginPage.java
@@ -1,19 +1,17 @@
 package toolShopDemo;
-
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.testng.Assert;
-
 public class LoginPage {
     WebDriver driver;
-
     public LoginPage(WebDriver driver) {
         this.driver = driver;
     }
 
     private By pageTitle = By.cssSelector(".col-lg-6.auth-form>h3");
-    private By userName = By.cssSelector(".input-group.mb-3>input[placeholder=\"Your E-mail *\"]");
-    private By password = By.xpath("//div[@class=\"input-group mb-3\"]/child::input[@formcontrolname=\"password\"]");
+
+    private By userName = By.cssSelector(".mb-3>input[placeholder=\"Enter your E-mail\"]");
+    private By password = By.xpath("//div[@class=\"mb-3\"]/child::input[@placeholder=\"Enter your Password\"]");
     private By login = By.xpath("//input[@data-test=\"login-submit\"]");
 
     public void verifyRegistration(){
@@ -21,7 +19,7 @@ public class LoginPage {
         Assert.assertTrue(titleViz);
 
         String url = driver.getCurrentUrl();
-        Assert.assertEquals(url,"https://practicesoftwaretesting.com/#/auth/login");
+        Assert.assertEquals(url, "https://practicesoftwaretesting.com/#/auth/login");
     }
 
     public void login(String userName,String password){
